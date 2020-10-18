94bf403ff88b4513f539b32c98f4f06e94768283
Marcel Marschner <marcel.marschner@outlook.com>
2020-10-18 23:09:03 +0200

    - simplified userId handling

--- src/actions/user/getCurrentUserId.js
+++ src/actions/user/getCurrentUserId.js
@@ -1,30 +1,23 @@
-import generateNewUserIdMockRepository from '../../repositories/user/generateNewUserIdMockRepository';
-import generateNewUserIdRepository from '../../repositories/user/generateNewUserIdRepository';
 import storageStub from '../../storage/storageStub';
-import config from '../../config';
+import { v4 as uuid } from 'uuid';
 
 const storage = localStorage || storageStub;
 
 export default () => (dispatch, getState) => new Promise((resolve) => {
     const { user } = getState();
 
-    let { currentUserId, guestUserId = '' } = user;
-    const guestUserIdPromise = guestUserId ? Promise.resolve(guestUserId) : new Promise((resolve) => {
-        const repository = (config.useMocks ?? true) ? generateNewUserIdMockRepository() : generateNewUserIdRepository();
-        repository.then((userId) => {
-            dispatch({
-                type: 'SET_GUEST_USER_ID',
-                userId
-            });
+    let { currentUserId, guestUserId } = user;
+    if (!guestUserId) {
+        guestUserId = uuid();
 
-            resolve(userId);
+        dispatch({
+            type: 'SET_GUEST_USER_ID',
+            userId: guestUserId
         });
-    });
+    }
 
-    guestUserIdPromise.then((guestUserId) => {
-        storage.setItem('guestUserId', guestUserId);
-        storage.setItem('currentUserId', currentUserId);
+    storage.setItem('guestUserId', guestUserId);
+    storage.setItem('currentUserId', currentUserId);
 
-        resolve(currentUserId || guestUserId);
-    });
+    resolve(currentUserId || guestUserId);
 });
--- src/repositories/wishlist/addWishlistItemRepository.js
+++ src/repositories/wishlist/addWishlistItemRepository.js
@@ -1,7 +1,6 @@
 import Axios from 'axios';
 
 export default (userId, itemId) => new Promise((resolve) => {
-    console.log('hi');
     Axios.put('http://localhost:3002/wishlist-item', { userId, itemId })
         .then(response => {
             resolve();
